20676095f88067d61f2df1e651a6a0c7683e47dd
Michaela Brydon <brydon1@kenyon.edu>
2020-09-22 11:11:28 -0400

    Make more general

--- app.py
+++ app.py
@@ -34,7 +34,10 @@ class App:
 if __name__ == '__main__':
     import sys
     config_file = sys.argv[1]
-    #file_name = sys.argv[2]
     app = App.configure(config_file)
-    temperatures_by_hour = app.read()
+    try:
+        file_name = sys.argv[2]
+        temperatures_by_hour = app.read(file_name = file_name)
+    except IndexError:
+        temperatures_by_hour = app.read()
     app.draw(temperatures_by_hour)
